8c0346ca8963630967fdc69c4f0f81fd0983ff7d
mydle19 <19mydle@gmail.com>
2022-10-10 21:10:03 -0400

    added printline

--- src/test/java/OtherTest.java
+++ src/test/java/OtherTest.java
@@ -1,6 +1,6 @@
 public class OtherTest {
     public static void main(String[] args) {
         System.out.println("I'm a software engineer");
-
+        System.out.println("the market is performing really poorly this year");
     }
 }
